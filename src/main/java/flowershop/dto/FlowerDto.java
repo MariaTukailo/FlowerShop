45cfc3c5ecee105198d25363a66287e12257cad9
TukailoMaria <tukajlomaria6@gmail.com>
2026-03-15 22:40:32 +0300

    4 lab

--- src/main/java/flowershop/dto/FlowerDto.java
+++ src/main/java/flowershop/dto/FlowerDto.java
@@ -1,13 +1,14 @@
 package flowershop.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 
 @AllArgsConstructor
 @Data
@@ -18,18 +19,18 @@ public class FlowerDto {
     @Schema(description = "Уникальный идентификатор цветов", example = "1")
     private Long id;
 
-    @Schema(description = "Название цветов", example = "Роза")
     @NotBlank(message = "Название не может быть пустым")
+    @Schema(description = "Название цветов", example = "Роза")
     @Size(min = 2, max = 30, message = "Название должно быть от 2 до 30 символов")
     private String name;
 
-    @Schema(description = "Статус активности цветов", example = "true")
     @NotNull(message = "Укажите, активен ли цветок (true/false)")
+    @Schema(description = "Статус активности цветов", example = "true")
     private boolean active;
 
+    @Positive(message = "Цена должна быть больше нуля")
     @Schema(description = "Цена цветов", example = "11")
     @NotNull(message = "Укажите цену")
-    @Positive(message = "Цена должна быть больше нуля")
     private double price;
 
     @Schema(description = "Цвет", example = "красный")
