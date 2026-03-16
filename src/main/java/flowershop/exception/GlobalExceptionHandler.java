a8a107738628bbccc628f5b897a8d5658c0da278
TukailoMaria <tukajlomaria6@gmail.com>
2026-03-16 12:29:34 +0300

    4new

--- src/main/java/flowershop/exception/GlobalExceptionHandler.java
+++ src/main/java/flowershop/exception/GlobalExceptionHandler.java
@@ -3,28 +3,22 @@ package flowershop.exception;
 import flowershop.dto.ErrorDto;
 import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import java.time.LocalDateTime;
+
 
 @Hidden
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDto> handleGenericException(Exception exception) {
-        log.error("Произошла ошибка с кодом {}", exception.getMessage());
-        ErrorDto error = new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-                exception.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                LocalDateTime.now());
 
-        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+
 
     @ExceptionHandler(jakarta.persistence.EntityNotFoundException.class)
     public ResponseEntity<ErrorDto> handleNotFoundException(EntityNotFoundException exception) {
@@ -52,17 +46,49 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(jakarta.validation.ConstraintViolationException.class)
-    public ResponseEntity<ErrorDto> handleConstraintViolationException(jakarta.validation.ConstraintViolationException exception) {
-        log.error("Произошла ошибка  с кодом {}", exception.getMessage());
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDto> handleConstraintViolationException(ConstraintViolationException exception) {
+        log.error("Ошибка валидации: {}", exception.getMessage());
+
+        String message = exception.getConstraintViolations()
+                .stream()
+                .map(violation -> violation.getPropertyPath() + ": " + violation.getMessage())
+                .findFirst()
+                .orElse("Ошибка валидации");
 
         ErrorDto error = new ErrorDto(
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
-                exception.getMessage(),
+                message,
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now()
         );
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleGenericException(Exception exception) {
+        log.error("Произошла ошибка с кодом {}", exception.getMessage());
+        ErrorDto error = new ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                exception.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now());
+
+        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.error(" Ошибка валидации:   {}", exception.getMessage());
+
+
+        ErrorDto error = new ErrorDto(
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "Ошибка валидации: " + exception.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now()
+        );
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
