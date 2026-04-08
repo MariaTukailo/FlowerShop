78f792ac7199e8618784853969483c04d1008798
TukailoMaria <tukajlomaria6@gmail.com>
2026-04-08 14:55:52 +0300

    8lab

--- flower_shop/src/api.jsx
+++ flower_shop/src/api.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://flowershop-production-f107.up.railway.app'
+    baseURL: window.location.hostname === 'localhost'
+        ? 'http://localhost:8080'
+        : 'https://flowershop-production-f107.up.railway.app'
 });
 
 export default api;
